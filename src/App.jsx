a4d1002ba1d9fd6067db70324a864bc0fa582eb0
Alex <95061769+WhiteWr@users.noreply.github.com>
2022-06-14 20:44:51 +0300

    Create project
    1. Copy webpack build
    2. Download and setup icons, images, fonts files
    3. Create and link react components
    4. Write readme files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// s
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Form from './components/Form.jsx';
+
+export default function asdads() {
+  return (
+    <div className='wrapper'>
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  )
+}
